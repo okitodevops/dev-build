3948b7528064aea918232e9f2b35fdf959a148d6
Craig Thacker <craigthackerx@gmail.com>
2022-04-17 23:21:28 +0100

    update module

--- terraform/build.tf
+++ terraform/build.tf
@@ -13,7 +13,7 @@ module "network" {
   rg_name  = module.rg.rg_name
   location = module.rg.rg_location
 
-  vnet_name = "vnet-${var.short}-${var.loc}-${terraform.workspace}-01"
+  vnet_name     = "vnet-${var.short}-${var.loc}-${terraform.workspace}-01"
   vnet_location = module.network.vnet_location
 
   address_space   = ["10.0.0.0/16"]
@@ -26,4 +26,64 @@ module "network" {
   }
 
   tags = local.tags
+}
+
+module "nsg" {
+  source = "../../terraform-azurerm-nsg"
+
+  rg_name   = module.rg.rg_name
+  location  = module.rg.rg_location
+  nsg_name  = "nsg-${var.short}-${var.loc}-${terraform.workspace}"
+  subnet_id = element(module.network.subnets_ids, 0)
+
+  tags = module.rg.rg_tags
+}
+
+resource "azurerm_network_interface" "nic" {
+  count = module.win_vm.vm_amount
+
+  name                = "${module.win_vm.vm_name}${format("%02d", count.index + 1)}-nic"
+  resource_group_name = module.rg.rg_name
+  location            = module.rg.rg_location
+
+  enable_accelerated_networking = false
+
+  ip_configuration {
+    name                          = "${module.win_vm.vm_name}${format("%02d", count.index + 1)}-ipconfig"
+    primary                       = true
+    private_ip_address_allocation = "dynamic"
+    subnet_id                     = element(module.network.subnets_ids, 0)
+  }
+  tags = module.rg.rg_tags
+
+  timeouts {
+    create = "5m"
+    delete = "10m"
+  }
+}
+
+module "win_vm" {
+  source = "../../terraform-azurerm-windows-vm"
+
+  rg_name  = module.rg.rg_name
+  location = module.rg.rg_location
+
+  vm_amount          = 2
+  vm_hostname        = "vm${var.short}${var.loc}${terraform.workspace}${format("%02d", count.index + 1)}"
+  vm_size            = "Standard_B2ms"
+  vm_os_simple       = "WindowsServer2019"
+  vm_os_disk_size_gb = "127"
+
+  admin_username = "LibreDevOpsAdmin"
+  admin_password = data.azurerm_key_vault_secret.mgmt_local_admin_pwd.value
+
+  nic_ids = [
+    azurerm_network_interface.nic[count.index].id
+  ]
+
+  availability_zone    = "alternate"
+  storage_account_type = "Standard_LRS"
+  identity_type        = "SystemAssigned"
+
+  tags = module.rg.rg_tags
 }
