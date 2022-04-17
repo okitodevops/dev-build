80cd733631e3bd9c40569b3962ae60df3bd890b2
Craig Thacker <craigthackerx@gmail.com>
2022-04-17 21:33:40 +0100

    update name

--- terraform/environment.tf
+++ terraform/environment.tf
@@ -10,7 +10,7 @@ variable "env" {
 variable "loc" {
   description = "The shorthand name of the Azure location, for example, for UK South, use uks.  For UK West, use ukw. Normally passed as TF_VAR in pipeline"
   type        = string
-  default     = "ukw"
+  default     = "euw"
 }
 
 variable "Regions" {
@@ -19,6 +19,7 @@ variable "Regions" {
     uks = "UK South"
     ukw = "UK West"
     eus = "East US"
+    euw = "West Europe"
   }
   description = "Converts shorthand name to longhand name via lookup on map list"
 }
