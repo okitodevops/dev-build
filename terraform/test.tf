921077c722c49c431a1f0ba0a4a47a91f68623f5
Craig Thacker <craigthackerx@gmail.com>
2022-04-17 21:11:13 +0100

    update

--- terraform/test.tf
+++ terraform/test.tf
@@ -1,69 +1,30 @@
-variable "environment" {
-  default     = "prd"
-  type        = string
-  description = "Used as an alternative to terraform.workspace"
+module "rg" {
+  source = "registry.terraform.io/libre-devops/rg/azurerm"
+
+  rg_name    = "rg-${var.short}-${var.loc}-${terraform.workspace}"
+  location   = local.location
+  lock_level = "CanNotDelete"
+  tags       = local.tags
 }
 
-locals {
+module "network" {
+  source = "registry.terraform.io/libre-devops/network/azurerm"
 
-  names = {
-    key0 = var.environment         // prd
-    key1 = "${var.environment}-vm" // prd-vm
-    key2 = "prd-biscuit"
-    key3 = "tst_pizza"
-  }
-}
+  rg_name  = module.rg.rg_name
+  location = module.rg.rg_location
 
-resource "azurerm_resource_group" "test_rg" {
-  for_each = {
-    for key, value in local.names : key => value
-    if length(regexall("${var.environment}-", value)) > 0 // Checks the values of the map called local.names, if the any value of that map contains the name "prd-" followed by anything else, then make a resource group for it, with that value of the map as the name of the resource group.  If no match is found, do nothing.
-  }
-  location = local.location
-  name     = each.value // makes 2 rgs, prd-vm and prd-biscuit
-}
+  vnet_name = "vnet-${var.short}-${var.loc}-${terraform.workspace}"
+  vnet_location = module.network.vnet_location
 
-// Use local or output from within a module to keep tidy, you could do this in-line but its a bad idea
-locals {
-  resource_group_locations = {
-    for key, value in element(azurerm_resource_group.test_rg[*], 0) : key => value.location
-  }
-
-  resource_group_name = {
-  for key, value in element(azurerm_resource_group.test_rg[*], 0) : key => value.name
-  }
-}
-
-resource "azurerm_application_security_group" "example" {
-  name                = "libre-devops-asg"
-  location            = element(values(local.resource_group_locations) , 0) // filters first element and gets value = uksouth
-  resource_group_name = element(values(local.resource_group_name) , 0) // filters second element and gets value = prd-biscuit
-
-  tags = {
-    Hello = "World"
-  }
-}
-
-module "vnet" {
-  source = "./test-modules/terraform-azurerm-network"
-
-  rg_name  = element(values(local.resource_group_name), 0)
-  location = element(values(local.resource_group_locations), 0)
   address_space   = ["10.0.0.0/16"]
   subnet_prefixes = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  subnet_names    = ["subnet1", "subnet2", "subnet3"]
+  subnet_names    = ["sn1-${module.network.vnet_name}", "sn2-${module.network.vnet_name}", "sn3-${module.network.vnet_name}"]
 
   subnet_service_endpoints = {
     subnet2 = ["Microsoft.Storage", "Microsoft.Sql"],
     subnet3 = ["Microsoft.AzureActiveDirectory"]
   }
 
-    tags = {
-      environment = "dev"
-      costcenter  = "it"
-    }
-}
+  tags = local.tags
 
-output "subnet" {
-  value = module.vnet.subnets_name_to_id
 }
