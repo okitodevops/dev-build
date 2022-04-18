04226675aa6ae9aab8ffff5f4f5cfc429fb91c0f
Craig Thacker <craigthackerx@gmail.com>
2022-04-18 20:52:25 +0100

    update build

--- terraform/data-mgmt.tf
+++ terraform/data-mgmt.tf
@@ -16,3 +16,8 @@ data "azurerm_key_vault_secret" "mgmt_local_admin_pwd" {
   key_vault_id = data.azurerm_key_vault.mgmt_kv.id
   name         = "Local${title(var.short)}Admin${title(terraform.workspace)}Pwd"
 }
+
+data "azurerm_user_assigned_identity" "mgmt_user_assigned_id" {
+  name                = "id-${var.short}-${var.loc}-${terraform.workspace}-mgt-01"
+  resource_group_name = data.azurerm_resource_group.mgmt_rg.name
+}
