9e3bcf524720c6b250c9d6592b18d401f44c64f8
Craig Thacker <craigthackerx@gmail.com>
2022-04-24 00:33:27 +0100

    update module

--- terraform/build.tf
+++ terraform/build.tf
@@ -265,7 +265,7 @@ module "sa" {
 #}
 #
 data "http" "user_ip" {
-  url = "https://checkip.amazonaws.com" // If running locally, running this block will fetch your outbound public IP of your home/office/ISP/VPN and add it.  It will add the hosted agent etc if running from Microsoft/GitLab
+  url = "https://ipv4.icanhazip.com" // If running locally, running this block will fetch your outbound public IP of your home/office/ISP/VPN and add it.  It will add the hosted agent etc if running from Microsoft/GitLab
 }
 
 #
