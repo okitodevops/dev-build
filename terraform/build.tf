26c7f01edf18091fbc8460ff48aaac3e79136842
Craig Thacker <craigthackerx@gmail.com>
2022-04-24 12:22:08 +0100

    Update

--- terraform/build.tf
+++ terraform/build.tf
@@ -67,32 +67,9 @@ module "sa" {
     }
   }
 
-  custom_domain = {
-    "libredevops.org" = {
-      use_subdomain = false
-    }
-  }
-
-  #  queue_properties = {
-  #    queue_properties = {
-  #
-  #      #    queue_properties_logging = {
-  #      #      settings = {
-  #      #        delete_enabled        = false
-  #      #        read_enabled          = true
-  #      #        write-enabled         = true
-  #      #        version               = "1.0"
-  #      #        retention_policy_days = "10"
-  #      #      }
-  #      #    }
-  #
-  #      queue_cors_rule = {
-  #        allowed_headers    = ["*"]
-  #        allowed_methods    = ["GET", "DELETE"]
-  #        allowed_origins    = ["*"]
-  #        exposed_headers    = ["*"]
-  #        max_age_in_seconds = 5
-  #      }
+  #  custom_domain = {
+  #    "libredevops.org" = {
+  #      use_subdomain = false
   #    }
   #  }
 
