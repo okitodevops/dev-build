6535a5e198bdd59d90f9ff0235ff30a926032c3c
Craig Thacker <craigthackerx@gmail.com>
2022-04-24 15:38:03 +0100

    Update

--- terraform/build.tf
+++ terraform/build.tf
@@ -5,7 +5,7 @@ module "rg" {
   location = local.location                                            // compares var.loc with the var.regions var to match a long-hand name, in this case, "euw", so "westeurope"
   tags     = local.tags
 
-  #  lock_level = "CanNotDelete" // Do not set this value to skip lock
+    lock_level = "CanNotDelete" // Do not set this value to skip lock
 }
 
 module "network" {
@@ -31,16 +31,12 @@ module "network" {
 module "nsg" {
   source = "registry.terraform.io/libre-devops/nsg/azurerm"
 
-  for_each = {
-    for key, value in module.network.subnets_ids : key => value // Creates a key-value pair of subnet_ids with the keys being the subnet name and value being the id, creates a default NSG and assigns to each
-  }
-
   rg_name  = module.rg.rg_name
   location = module.rg.rg_location
   tags     = module.rg.rg_tags
 
-  nsg_name  = "nsg-${each.key}" // nsg-sn*-vnet-ldo-euw-dev-01
-  subnet_id = each.value        // Adds NSG to all subnets
+  nsg_name  = "nsg-${element(values(module.network.subnets_names), 0)}" // nsg-sn*-vnet-ldo-euw-dev-01
+  subnet_id = element(values(module.network.subnets_ids) , 0)        // Adds NSG to all subnets
 }
 
 // This module does not consider for CMKs and allows the users to manually set bypasses
@@ -170,69 +166,69 @@ module "private_lb" {
 
 // Fix error which causes security errors to be flagged by TFSec, public egress is needed for Azure Bastion to function, its kind of the point :)
 #tfsec:ignore:azure-network-no-public-egress
-#module "bastion" {
-#  source = "registry.terraform.io/libre-devops/bastion/azurerm"
-#
-#  vnet_rg_name = module.network.vnet_rg_name
-#  vnet_name    = module.network.vnet_name
-#  tags         = module.rg.rg_tags
-#
-#  bas_subnet_iprange = "10.0.4.0/28" // /28 is minimum for AzureBastionSubnet
-#
-#  bas_nsg_name     = "nsg-bas-${var.short}-${var.loc}-${terraform.workspace}-01" // nsg-bas-ldo-euw-dev-01
-#  bas_nsg_location = module.rg.rg_location
-#  bas_nsg_rg_name  = module.rg.rg_name
-#
-#  bas_pip_name              = "pip-bas-${var.short}-${var.loc}-${terraform.workspace}-01" //pip-bas-ldo-euw-dev-01
-#  bas_pip_location          = module.rg.rg_location
-#  bas_pip_rg_name           = module.rg.rg_name
-#  bas_pip_allocation_method = "Static"
-#  bas_pip_sku               = "Standard"
-#
-#  bas_host_name          = "bas-${var.short}-${var.loc}-${terraform.workspace}-01" // bas-ldo-euw-dev-01
-#  bas_host_location      = module.rg.rg_location
-#  bas_host_rg_name       = module.rg.rg_name
-#  bas_host_ipconfig_name = "bas-${var.short}-${var.loc}-${terraform.workspace}-01-ipconfig" // bas-ldo-euw-dev-01-ipconfig
-#}
+module "bastion" {
+  source = "registry.terraform.io/libre-devops/bastion/azurerm"
+
+  vnet_rg_name = module.network.vnet_rg_name
+  vnet_name    = module.network.vnet_name
+  tags         = module.rg.rg_tags
+
+  bas_subnet_iprange = "10.0.4.0/28" // /28 is minimum for AzureBastionSubnet
+
+  bas_nsg_name     = "nsg-bas-${var.short}-${var.loc}-${terraform.workspace}-01" // nsg-bas-ldo-euw-dev-01
+  bas_nsg_location = module.rg.rg_location
+  bas_nsg_rg_name  = module.rg.rg_name
+
+  bas_pip_name              = "pip-bas-${var.short}-${var.loc}-${terraform.workspace}-01" //pip-bas-ldo-euw-dev-01
+  bas_pip_location          = module.rg.rg_location
+  bas_pip_rg_name           = module.rg.rg_name
+  bas_pip_allocation_method = "Static"
+  bas_pip_sku               = "Standard"
+
+  bas_host_name          = "bas-${var.short}-${var.loc}-${terraform.workspace}-01" // bas-ldo-euw-dev-01
+  bas_host_location      = module.rg.rg_location
+  bas_host_rg_name       = module.rg.rg_name
+  bas_host_ipconfig_name = "bas-${var.short}-${var.loc}-${terraform.workspace}-01-ipconfig" // bas-ldo-euw-dev-01-ipconfig
+}
 
 // This module does not consider for log analytics oms agent, but tfsec warns anyway.  Code exists to enable it should you wish by check is tabled
 #tfsec:ignore:azure-container-logging
-#module "aks" {
-#  source = "registry.terraform.io/libre-devops/aks/azurerm"
-#
-#  rg_name  = module.rg.rg_name
-#  location = module.rg.rg_location
-#  tags     = module.rg.rg_tags
-#
-#  aks_name                = "aks-${var.short}-${var.loc}-${terraform.workspace}-01" // aks-ldo-euw-dev-01
-#  admin_username          = "LibreDevOpsAdmin"
-#  ssh_public_key          = data.azurerm_ssh_public_key.mgmt_ssh_key.public_key // Created with Libre DevOps PreRequisite Script
-#  kubernetes_version      = "1.22.6"
-#  dns_prefix              = "ldo"
-#  sku_tier                = "Free"
-#  private_cluster_enabled = true
-#  enable_rbac             = true
-#
-#  default_node_enable_auto_scaling  = false
-#  default_node_orchestrator_version = "1.22.6"
-#  default_node_pool_name            = "lbdopool"
-#  default_node_vm_size              = "Standard_B2ms"
-#  default_node_os_disk_size_gb      = "127"
-#  default_node_subnet_id            = element(values(module.network.subnets_ids), 2) // places in sn3-vnet-ldo-euw-dev-01
-#  default_node_availability_zones   = ["1"]
-#  default_node_count                = "1"
-#  default_node_agents_min_count     = null
-#  default_node_agents_max_count     = null
-#
-#  identity_type = "UserAssigned" // Created with Libre DevOps PreRequisite Script
-#  identity_ids  = [data.azurerm_user_assigned_identity.mgmt_user_assigned_id.id]
-#
-#  network_plugin                 = "azure"
-#  network_policy                 = "azure"
-#  net_profile_service_cidr       = "10.0.5.0/24"
-#  net_profile_dns_service_ip     = "10.0.5.10"
-#  net_profile_docker_bridge_cidr = "172.17.0.1/16"
-#}
+module "aks" {
+  source = "registry.terraform.io/libre-devops/aks/azurerm"
+
+  rg_name  = module.rg.rg_name
+  location = module.rg.rg_location
+  tags     = module.rg.rg_tags
+
+  aks_name                = "aks-${var.short}-${var.loc}-${terraform.workspace}-01" // aks-ldo-euw-dev-01
+  admin_username          = "LibreDevOpsAdmin"
+  ssh_public_key          = data.azurerm_ssh_public_key.mgmt_ssh_key.public_key // Created with Libre DevOps PreRequisite Script
+  kubernetes_version      = "1.22.6"
+  dns_prefix              = "ldo"
+  sku_tier                = "Free"
+  private_cluster_enabled = true
+  enable_rbac             = true
+
+  default_node_enable_auto_scaling  = false
+  default_node_orchestrator_version = "1.22.6"
+  default_node_pool_name            = "lbdopool"
+  default_node_vm_size              = "Standard_B2ms"
+  default_node_os_disk_size_gb      = "127"
+  default_node_subnet_id            = element(values(module.network.subnets_ids), 2) // places in sn3-vnet-ldo-euw-dev-01
+  default_node_availability_zones   = ["1"]
+  default_node_count                = "1"
+  default_node_agents_min_count     = null
+  default_node_agents_max_count     = null
+
+  identity_type = "UserAssigned" // Created with Libre DevOps PreRequisite Script
+  identity_ids  = [data.azurerm_user_assigned_identity.mgmt_user_assigned_id.id]
+
+  network_plugin                 = "azure"
+  network_policy                 = "azure"
+  net_profile_service_cidr       = "10.0.5.0/24"
+  net_profile_dns_service_ip     = "10.0.5.10"
+  net_profile_docker_bridge_cidr = "172.17.0.1/16"
+}
 
 module "win_vm" {
   source = "registry.terraform.io/libre-devops/windows-vm/azurerm"
@@ -316,38 +312,34 @@ module "run_command_lnx" {
 }
 
 // Allow Inbound Access from Bastion to the entire virtual network
-#resource "azurerm_network_security_rule" "AllowSSHRDPInboundFromBasSubnet" {
-#  name                         = "AllowBasSSHRDPInbound"
-#  priority                     = 400
-#  direction                    = "Inbound"
-#  access                       = "Allow"
-#  protocol                     = "Tcp"
-#  source_port_range            = "*"
-#  destination_port_ranges      = ["22", "3389"]
-#  source_address_prefixes      = module.bastion.bas_subnet_ip_range
-#  destination_address_prefixes = module.network.vnet_address_space
-#  resource_group_name          = module.rg.rg_name
-#  network_security_group_name  = module.nsg.nsg_name
-#}
+resource "azurerm_network_security_rule" "AllowSSHRDPInboundFromBasSubnet" {
+  name                         = "AllowBasSSHRDPInbound"
+  priority                     = 400
+  direction                    = "Inbound"
+  access                       = "Allow"
+  protocol                     = "Tcp"
+  source_port_range            = "*"
+  destination_port_ranges      = ["22", "3389"]
+  source_address_prefixes      = module.bastion.bas_subnet_ip_range
+  destination_address_prefixes = module.network.vnet_address_space
+  resource_group_name          = module.rg.rg_name
+  network_security_group_name  = module.nsg.nsg_name
+}
 
 data "http" "user_ip" {
   url = "https://ipv4.icanhazip.com" // If running locally, running this block will fetch your outbound public IP of your home/office/ISP/VPN and add it.  It will add the hosted agent etc if running from Microsoft/GitLab
 }
 
-// Allow Inbound Access from your hypothetical home IP - you may not want this.
-#resource "azurerm_network_security_rule" "AllowSSHRDPInboundFromHomeSubnet" {
-#  for_each = {
-#    for key, value in module.network.subnets_ids : key => value
-#  }
-#  name                         = "AllowBasSSHRDPFromHomeInbound"
-#  priority                     = 405
-#  direction                    = "Inbound"
-#  access                       = "Allow"
-#  protocol                     = "Tcp"
-#  source_port_range            = "*"
-#  destination_port_ranges      = ["22", "3389"]
-#  source_address_prefixes      = [chomp(data.http.user_ip.body)] // Chomp function removes a heredoc response from http user ip response
-#  destination_address_prefixes = module.network.vnet_address_space
-#  resource_group_name          = module.rg.rg_name
-#  network_security_group_name  = element(values(module.nsg.nsg_name), 0)
-#}
+resource "azurerm_network_security_rule" "AllowSSHRDPInboundFromHomeSubnet" {
+  name                         = "AllowBasSSHRDPFromHomeInbound"
+  priority                     = 405
+  direction                    = "Inbound"
+  access                       = "Allow"
+  protocol                     = "Tcp"
+  source_port_range            = "*"
+  destination_port_ranges      = ["22", "3389"]
+  source_address_prefixes      = [chomp(data.http.user_ip.body)] // Chomp function removes a heredoc response from http user ip response
+  destination_address_prefixes = module.network.vnet_address_space
+  resource_group_name          = module.rg.rg_name
+  network_security_group_name  = element(values(module.nsg.nsg_name, 0)
+}
